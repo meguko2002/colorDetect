80bfacaee9173a6e4543e1da4755ebb3bbfef62e
yama <meguko2002@gmail.com>
2018-10-09 23:19:10 +0900

    色選択対応、plt.subplotで元画像と結果画像を併記

--- colorDetect.py
+++ colorDetect.py
@@ -2,37 +2,58 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 # 設定
-color = 1 # 検出する色を指定（1=青,2=緑,3=赤,0=黒）
+color = 3# 検出する色を指定（1=青,2=緑,3=赤）
+
+def color_pick(color):
+    if color == 1:
+        # HSV色空間に変換
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # 青色の検出
+        hsv_min = np.array([110,150,150])
+        hsv_max = np.array([130,255,255])
+        mask = cv2.inRange(hsv, hsv_min, hsv_max)
+    elif color == 2:
+        # HSV色空間に変換
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # 緑色の検出
+        hsv_min = np.array([30,50,50])
+        hsv_max = np.array([80,255,255])
+        mask = cv2.inRange(hsv, hsv_min, hsv_max)
+    elif color == 3:
+        # HSV色空間に変換
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # 赤色の検出
+        hsv_min = np.array([0,65,65])
+        hsv_max = np.array([10,255,255])
+        mask = cv2.inRange(hsv, hsv_min, hsv_max)
+        hsv_min = np.array([170,65,65])
+        hsv_max = np.array([180,255,255])
+        mask += cv2.inRange(hsv, hsv_min, hsv_max)
+    return mask
 
 img = cv2.imread('image1.jpg')
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# define range of blue color in HSV
-lower_red = np.array([0, 50, 50])
-upper_red = np.array([10, 255, 255])
-mask = cv2.inRange(hsv, lower_red, upper_red)
-lower_red = np.array([160, 50, 50])
-upper_red = np.array([180, 255, 255])
-mask += cv2.inRange(hsv, lower_red, upper_red)
 # Bitwise-AND mask and original image
+mask = color_pick(color)
 res = cv2.bitwise_and(img, img, mask=mask)
 
 image, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 print("個数 =",len(contours))
-print(type(contours))
 # 領域の重心を計算
 P = np.empty([len(contours),2])
 print(type(P))
 for i in range(len(contours)):
-# for i in contours:
     cnt = contours[i]
-    M = cv2.moments(cnt)
-    P[i][0] = int(M['m10'] / M['m00'])
-    P[i][1] = int(M['m01'] / M['m00'])
+    try:
+        M = cv2.moments(cnt)
+        P[i][0] = int(M['m10'] / M['m00'])
+        P[i][1] = int(M['m01'] / M['m00'])
+    except ZeroDivisionError:
+        # たまにゼロ割になってしまうケースが有るので対処
+        print("ZeroDivisionError!!")
 
-    # 検出した領域を表示
-    # cv2.drawContours(img, cnt, -1, (0, 255, 0), 3)
 print(P)
-plt.imshow(res)
-plt.colorbar()
+
+plt.subplot(1,2,1),plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+plt.subplot(1,2,2),plt.imshow(cv2.cvtColor(res,cv2.COLOR_BGR2RGB))
 plt.show()
