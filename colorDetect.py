6571b10ea2032d834989eb8a443e42b76f1e7395
yama <meguko2002@gmail.com>
2018-10-23 23:40:20 +0900

    動画対応

--- colorDetect.py
+++ colorDetect.py
@@ -1,60 +1,70 @@
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-# 設定
-color = 3# 検出する色を指定（1=青,2=緑,3=赤）
-
-def color_pick(color):
-    if color == 1:
+import cv2
+def color_pick(color,image):
+    if color == 1:        # 青色の検出
         # HSV色空間に変換
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        # 青色の検出
-        hsv_min = np.array([110,150,150])
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        hsv_min = np.array([100,60,150])
         hsv_max = np.array([130,255,255])
         mask = cv2.inRange(hsv, hsv_min, hsv_max)
-    elif color == 2:
+    elif color == 2:        # 緑色の検出
         # HSV色空間に変換
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        # 緑色の検出
-        hsv_min = np.array([30,50,50])
-        hsv_max = np.array([80,255,255])
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        hsv_min = np.array([20,60,50])
+        hsv_max = np.array([100,255,255])
         mask = cv2.inRange(hsv, hsv_min, hsv_max)
-    elif color == 3:
+    elif color == 3:        # 赤色の検出
         # HSV色空間に変換
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        # 赤色の検出
-        hsv_min = np.array([0,65,65])
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        hsv_min = np.array([0,60,65])
         hsv_max = np.array([10,255,255])
         mask = cv2.inRange(hsv, hsv_min, hsv_max)
-        hsv_min = np.array([170,65,65])
+        hsv_min = np.array([170,60,65])
         hsv_max = np.array([180,255,255])
         mask += cv2.inRange(hsv, hsv_min, hsv_max)
     return mask
 
-img = cv2.imread('image1.jpg')
-
-# Bitwise-AND mask and original image
-mask = color_pick(color)
-res = cv2.bitwise_and(img, img, mask=mask)
-
-image, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-print("個数 =",len(contours))
-# 領域の重心を計算
-P = np.empty([len(contours),2])
-
-fig, (ax1, ax2) = plt.subplots(1, 2)
-for i in range(len(contours)):
-    cnt = contours[i]
+def detect_center_of_gravity(cnt):
     try:
         M = cv2.moments(cnt)
-        P[i][0] = int(M['m10'] / M['m00'])
-        P[i][1] = int(M['m01'] / M['m00'])
-        ax2.text(P[i][0], P[i][1], P[i], color='g')
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        return cx, cy
     except ZeroDivisionError:
         # たまにゼロ割になってしまうケースが有るので対処
         print("ZeroDivisionError!!")
 
-print(P)
-ax1.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
-ax2.imshow(cv2.cvtColor(res,cv2.COLOR_BGR2RGB))
-plt.show()
+
+color = 3  #1=青,2=緑,3=赤
+cap = cv2.VideoCapture(0)
+kernel = np.ones((5,5),np.uint8)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    mask = color_pick(color, frame)
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    image, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    # print("個数 =", len(contours))
+
+    # 最大の領域を選定
+    max_area = 0
+    best_cnt = None
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if max_area < area:
+            best_cnt = cnt
+            max_area = area
+
+    cv2.imshow('mask',mask)
+    cv2.imshow('opening',opening)
+    imC = frame.copy()
+    imC = cv2.drawContours(imC, [best_cnt],0, (0, 255, 0), 3)
+    cx, cy = detect_center_of_gravity(best_cnt)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(imC, str(cx)+","+str(cy), (cx, cy), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.imshow('origin',imC)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
